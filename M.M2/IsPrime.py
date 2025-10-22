02d4a9d1e180f767cf02cb36e3e429e943405e47
Faizeen-Nur-Arzan <faizeennurarzan2014@gmail.com>
2025-10-22 18:40:14 +0600

    IsPrime

---
+++ M.M2/IsPrime.py
@@ -0,0 +1,10 @@
+def IsPrime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5)+1):
+        if num % i == 0:
+            return False
+    return True
+
+num = int(input("Enter any rational number: "))
+print(IsPrime(num))
