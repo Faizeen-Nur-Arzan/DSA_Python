b5bb7531ccfd61336c0741b5a523f68c042887f0
Faizeen-Nur-Arzan <faizeennurarzan2014@gmail.com>
2026-03-04 15:45:35 +0600

    Add files via upload

---
+++ Linked_list/LL1/simple_linked_list.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def append(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+        
+        last = self.head
+        while last.next is not None:
+            last = last.next
+        
+        last.next = new_node
+    
+    def prepend(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
